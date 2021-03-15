68b5f684e2830b3a74fe151a5d0bfa7aed674b94
Arthur Carneiro <arthur.menezes@presidencia.gov.br>
2021-03-15 16:51:56 -0300

    Add: Divisao em components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,43 @@
-export function Content() {
+import { MovieCard } from "../components/MovieCard";
+
+interface ContentProps {
+  selectedGenre: {
+    title: string;
+  };
+  movies: Array<{
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+
+  return (
+    <>
+      <header>
+        <span className="category">
+          Categoria:<span> {props.selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
